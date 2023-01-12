3818fd32349509d44ea08641defe40d4017267e7
unknown <marcigaglia.simone@live.com>
2023-01-12 13:09:52 +0100

    implement recursive region plotting

--- exampleUsage.py
+++ exampleUsage.py
@@ -4,57 +4,64 @@ import os.path
 import pandas as pd
 import pyvista as pv
 import pyvistaqt as pvqt
+import numpy as np
 
 # %% Define paths
 atlasFolder = "ccf2017"
-atlasResolution = 10  # available resolutions are 10,25,50 and 100 microns
+atlasResolution = 100  # available resolutions are 10,25,50 and 100 microns
 
 
 # %% Load atlas
 st = pd.read_csv(os.path.join(atlasFolder, 'structure_tree_safe_2017.csv'))
 ann = load.loadAtlas(atlasFolder, atlasResolution)
 
-# %% Create surfaces
+
+# %% Create brain surface
 brain = view.makeBrainContour(ann)
 
-hip = view.makeRegionByID(ann, st, 1089)
-pir = view.makeRegionByID(ann, st, 961)
 
+# %% Set up 3d plotter
 
-# %% Plot
+# Use pvqt.BackgroundPlotter for an interactive display or pv.Plotter to create
+# the animation under the %%Make video section
 
 pInt = pvqt.BackgroundPlotter(off_screen=False, window_size=(800, 600))
-# Alternatively use pv.Plotter but that is blocking (i.e. runs in the same thread)
-pInt.set_background('white')
-pInt.add_mesh(brain, color='black', opacity=.2, smooth_shading=True)
+# pInt = pv.Plotter(off_screen=True, window_size=(800, 600))
 
-pInt.add_mesh(hip, color='red', smooth_shading=True)
-pInt.add_mesh(pir, color='blue', smooth_shading=True)
 
+pInt.set_background('black')
+pInt.add_mesh(brain, color='white', opacity=.15, smooth_shading=True)
+
+
+# %% Add region meshes
+regionIndices = [507, 145, 536, 549, 502]
+colours = ["4BC6B9", "73C1C6", "96C3CE", "A79AB2", "B57BA6"]
+annotations = ["Main olfactory bulb", "Fourth ventricle",
+               "Central amygdalar nucleus", "Thalamus", "Subiculum"]
+abbreviations = ["MOB", "V4", "CEA", "TH", "SUB"]
+
+for r, regionId in enumerate(regionIndices):
+    surf = view.makeRegionByID(ann, st, regionId)
+    pInt.add_mesh(surf, color=colours[r])
+
+
+# Uncomment one of the following two lines depending on what you want to appear in the legend
+legend_entries = list(zip(abbreviations, colours))
+#legend_entries = list(zip(annotations, colours))
+
+
+pInt.add_legend(legend_entries, bcolor=None)
 pInt.add_bounding_box()
 
-legend_entries = []
-legend_entries.append(['Hippocampal formation', 'red'])
-legend_entries.append(['Piriform cortex', 'blue'])
-pInt.add_legend(legend_entries, bcolor=None)
-pInt.show()
-
+pInt.show()                     # for interactive display
+# pInt.show(auto_close=False)   # for video making
 
 # %% Make video
-pVid = pv.Plotter(off_screen=True, window_size=(800, 600))
-pVid.set_background('white')
-pVid.add_mesh(brain, color='black', opacity=.2, smooth_shading=True)
 
-pVid.add_mesh(hip, color='red', smooth_shading=True)
-pVid.add_mesh(pir, color='blue', smooth_shading=True)
-
-pVid.add_bounding_box()
-pVid.add_legend(legend_entries, bcolor=None)
-pVid.show(auto_close=False)
-viewup = [0, 0, 1]
-path = pVid.generate_orbital_path(
-    factor=2.0, n_points=360, viewup=viewup, shift=90)
-pVid.open_gif("orbit.gif")
-# pVid.open_movie("orbit.mp4", quality=5, framerate=15)
-pVid.orbit_on_path(path, write_frames=True, viewup=viewup, step=0.05)
-pVid.close()
+# viewup = [0, 0, 1]
+# path = pInt.generate_orbital_path(
+#     factor=2.0, n_points=360, viewup=viewup, shift=np.size(ann, 2)*2)
+# pInt.open_gif("orbit_10.gif")
+# # pInt.open_movie("orbit.mp4", quality=5, framerate=15)
+# pInt.orbit_on_path(path, write_frames=True, viewup=viewup, step=0.05)
+# pInt.close()
